ae807b043c6015839b39bb91afbfc504794c34df
Mikelesnr <mwanza.n.m@gmail.com>
2023-12-31 18:17:45 +0200

    mailer added

--- app/contacts/page.jsx
+++ app/contacts/page.jsx
@@ -1,7 +1,7 @@
-import Hero from "@components/Hero";
-import ContactForm from "@components/contactForm";
+import Hero from "../../components/Hero";
+import ContactForm from "../../components/contactForm";
 import Link from "next/link";
-import Banner from "@components/Banner";
+import Banner from "../../components/Banner";
 // import RoomsContainer from "@components/RoomsContainer";
 const Contacts = () => {
   return (
