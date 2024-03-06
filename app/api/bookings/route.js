1fc52e80ac601a67ea49c22792ca5aa386689e4b
Mikelesnr <mwanza.n.m@gmail.com>
2024-03-06 13:09:40 +0200

    booking system update

--- app/api/bookings/route.js
+++ app/api/bookings/route.js
@@ -1,74 +1,41 @@
-import prisma from "@/app/utils/connect";
-// import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/utils/authOptions";
+import prisma from "../../utils/connect";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
   try {
-    // if (!userId) {
-    //   return NextResponse.json({ error: "Unauthorized", status: 401 });
-    // }
+    const { firstName, lastName, roomName, email, checkIn, checkOut } =
+      await req.json();
 
-    const { name, email, room, checkIn, checkOut } = await req.json();
+    // checkIn = new Date(checkIn);
 
-    // const nameArr = splitString(assigned, " ");
-    // const fName = nameArr[0];
-    // const lName = nameArr[nameArr.length - 1];
-    // const records: any =
-    //   await prisma.$queryRaw`SELECT * FROM User WHERE firstName = ${fName} AND lastName = ${lName} `;
-    // const assignedId = records[0].id;
-    // const email = records[0].email;
-
-    //   const body = compileTaskCreateTemplate(fName, title, description);
-    //   await sendMail({
-    //     to: email,
-    //     subject: "New Task",
-    //     body: body,
-    //     attachments: [],
-    //   });
-
-    //   if (!title || !description || !date) {
-    //     return NextResponse.json({
-    //       error: "Missing required fields",
-    //       status: 400,
-    //     });
-    //   }
-
-    //   if (title.length < 3) {
-    //     return NextResponse.json({
-    //       error: "Title must be at least 3 characters long",
-    //       status: 400,
-    //     });
-    //   }
-
-    const booking = await prisma.task.create({
+    const booking = await prisma.booking.create({
       data: {
-        name,
+        firstName,
+        lastName,
+        roomName,
         email,
-        room,
         checkIn,
         checkOut,
       },
     });
 
-    return NextResponse.json(task);
+    return NextResponse.json(booking);
   } catch (error) {
-    console.log("ERROR CREATING TASK: ", error);
-    return NextResponse.json({ error: "Error creating task", status: 500 });
+    console.log("ERROR CREATING BOOKING: ", error);
+    return NextResponse.json({
+      error: "Error creating booking",
+      status: 500,
+    });
   }
 }
 
 export async function GET(req) {
   try {
-    if (!userId) {
-      return NextResponse.json({ error: "Unauthorized", status: 401 });
-    }
+    const bookings = await prisma.booking.findMany();
 
-    const bookings = await prisma.user.findMany();
-
-    return NextResponse.json(users);
+    return NextResponse.json(bookings);
   } catch (error) {
     console.log("ERROR GETTING TASKS: ", error);
-    return NextResponse.json({ error: "Error getting tasks", status: 500 });
+    return NextResponse.json({ error: "Error getting bookings", status: 500 });
   }
 }
--- components/bookingForm.js
+++ components/bookingForm.js
@@ -4,6 +4,46 @@ import Title from "./Title";
 import RoomsBooking from "./RoomsBooking";
 
 export default function BookingForm() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [roomName, setRoomName] = useState("");
+  const [checkIn, setCheckIn] = useState("");
+  const [checkOut, setCheckOut] = useState("");
+  const [numRooms, setNumRooms] = useState("");
+  const [numGuests, setNumGuests] = useState("");
+  const [phone, setPhone] = useState("");
+
+  function sendForm() {
+    let item = {
+      firstName,
+      lastName,
+      email,
+      checkIn,
+      checkOut,
+      roomName,
+      numGuests,
+      numRooms,
+    };
+
+    // //Fetching user registration api
+    // let result = await fetch("/api/mailer", {
+    //   method: "POST",
+    //   body: JSON.stringify(item),
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Accept: "application/json",
+    //   },
+    // });
+    // result = await result.json();
+    // console.log(result.message);
+    // setfirstName("");
+    // setSubject("");
+    // setEmail("");
+    // setMessage("");
+    console.log(item);
+  }
+
   return (
     <div>
       <div className="flex items-center justify-center p-12">
@@ -14,15 +54,17 @@ export default function BookingForm() {
               <div className="w-full px-3 sm:w-1/2">
                 <div className="mb-5">
                   <label
-                    for="fName"
+                    for="firstName"
                     className="mb-3 block text-base font-medium text-[#07074D]"
                   >
                     First Name
                   </label>
                   <input
-                    type="text"
-                    name="fName"
-                    id="fName"
+                    type="firstName"
+                    name="firstName"
+                    id="firstname"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value)}
                     placeholder="First Name"
                     className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                   />
@@ -31,15 +73,17 @@ export default function BookingForm() {
               <div className="w-full px-3 sm:w-1/2">
                 <div className="mb-5">
                   <label
-                    for="lName"
+                    for="lastName"
                     className="mb-3 block text-base font-medium text-[#07074D]"
                   >
                     Last Name
                   </label>
                   <input
-                    type="text"
-                    name="lName"
-                    id="lName"
+                    type="lastName"
+                    name="lastName"
+                    id="lastname"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value)}
                     placeholder="Last Name"
                     className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                   />
@@ -51,15 +95,17 @@ export default function BookingForm() {
               <div className="w-full px-3 sm:w-1/2">
                 <div className="mb-5">
                   <label
-                    for="date"
+                    for="numGuests"
                     className="mb-3 block text-base font-medium text-[#07074D]"
                   >
                     Number of Guests
                   </label>
                   <input
                     type="number"
-                    name="check-in"
-                    id="check-in"
+                    name="numGuests"
+                    id="numGuests"
+                    value={numGuests}
+                    onChange={(e) => setNumGuests(e.target.value)}
                     placeholder="0"
                     className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                   />
@@ -68,15 +114,17 @@ export default function BookingForm() {
               <div className="w-full px-3 sm:w-1/2">
                 <div className="mb-5">
                   <label
-                    for="date"
+                    for="numRooms"
                     className="mb-3 block text-base font-medium text-[#07074D]"
                   >
                     Number of Rooms
                   </label>
                   <input
                     type="number"
-                    name="check-out"
-                    id="check-out"
+                    name="numRooms"
+                    id="numRooms"
+                    value={numRooms}
+                    onChange={(e) => setNumRooms(e.target.value)}
                     placeholder="0"
                     className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                   />
@@ -88,7 +136,7 @@ export default function BookingForm() {
               <div className="w-full px-3 sm:w-1/2">
                 <div className="mb-5">
                   <label
-                    for="date"
+                    for="phone"
                     className="mb-3 block text-base font-medium text-[#07074D]"
                   >
                     Phone Number
@@ -97,6 +145,8 @@ export default function BookingForm() {
                     type="tel"
                     name="phone"
                     id="phone"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
                     placeholder="+263 772 000 000"
                     className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                   />
@@ -105,7 +155,7 @@ export default function BookingForm() {
               <div className="w-full px-3 sm:w-1/2">
                 <div className="mb-5">
                   <label
-                    for="date"
+                    for="email"
                     className="mb-3 block text-base font-medium text-[#07074D]"
                   >
                     Email
@@ -114,6 +164,8 @@ export default function BookingForm() {
                     type="email"
                     name="email"
                     id="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
                     placeholder="address@email.com"
                     className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                   />
@@ -125,15 +177,17 @@ export default function BookingForm() {
               <div className="w-full px-3 sm:w-1/2">
                 <div className="mb-5">
                   <label
-                    for="date"
+                    for="checkIn"
                     className="mb-3 block text-base font-medium text-[#07074D]"
                   >
                     Check In
                   </label>
                   <input
                     type="date"
-                    name="check-in"
-                    id="check-in"
+                    name="checkIn"
+                    id="checkIn"
+                    value={checkIn}
+                    onChange={(e) => setCheckIn(e.target.value)}
                     className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                   />
                 </div>
@@ -141,15 +195,17 @@ export default function BookingForm() {
               <div className="w-full px-3 sm:w-1/2">
                 <div className="mb-5">
                   <label
-                    for="date"
+                    for="checkOut"
                     className="mb-3 block text-base font-medium text-[#07074D]"
                   >
                     Check out
                   </label>
                   <input
                     type="date"
-                    name="check-out"
-                    id="check-out"
+                    name="checkOut"
+                    id="checkOut"
+                    value={checkOut}
+                    onChange={(e) => setCheckOut(e.target.value)}
                     className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
                   />
                 </div>
@@ -194,7 +250,8 @@ export default function BookingForm() {
 
             <div>
               <button
-                type="button"
+                type="submit"
+                formAction={sendForm}
                 data-te-ripple-init
                 data-te-ripple-color="light"
                 className="btn-form mb-6 inline-block w-full rounded px-6 pt-2.5 pb-2 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:bg-primary-600 hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:bg-primary-600 focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:bg-primary-700 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
