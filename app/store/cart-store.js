599c760cd7048f6a399276f34df433f26e0171d5
Mikelesnr <mwanza.n.m@gmail.com>
2024-07-29 15:57:45 +0200

    cart edit fix

--- app/store/cart-store.js
+++ app/store/cart-store.js
@@ -8,10 +8,19 @@ export const useCartStore = create((set) => ({
   addToCart: (room) => set((state) => ({ cart: [...state.cart, room] })),
   addToBookingCart: (booking) =>
     set((state) => ({ bookingCart: [...state.bookingCart, booking] })),
-  removeFromCart: (roomName) =>
-    set((state) => ({
-      cart: state.cart.filter((room) => room.name !== roomName),
-    })),
+  removeFromCart: (roomName) => {
+    set((state) => {
+      const indexToRemove = state.cart.findIndex(
+        (room) => room.name === roomName
+      );
+      if (indexToRemove !== -1) {
+        const updatedCart = [...state.cart];
+        updatedCart.splice(indexToRemove, 1); // Remove one item at the specified index
+        return { cart: updatedCart };
+      }
+      return state; // No change if room not found
+    });
+  },
   removeFromBookingCart: (roomName) =>
     set((state) => ({
       bookingCart: state.bookingCart.filter((room) => room !== roomName),
